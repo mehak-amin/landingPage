39891174a1ae8cfaaf481645e533e3e42ca7796f
Aamir Bashir <41386062+huzuhuzair@users.noreply.github.com>
2024-05-10 10:21:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // const BASE_URI = "http://tracker.ap-south-1.elasticbeanstalk.com/";
-const BASE_URI = "http://localhost:5050";
+const BASE_URI = "http://emp-monitoring.ap-south-1.elasticbeanstalk.com";
 
 export default BASE_URI;
